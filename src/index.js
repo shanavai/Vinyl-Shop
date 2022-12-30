b3de3676d647d7de0ee89de0ce0c76c9a4013472
i <iraklishanava01@gmail.com>
2022-12-31 02:39:00 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./theme";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./state";
+
+const state = configureStore({
+  reducer: { cart: cartReducer },
+});
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={state}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/scenes/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainCarousel from './MainCarousel'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <MainCarousel/> 
+    </div>
+  )
+}
+
+export default Home
